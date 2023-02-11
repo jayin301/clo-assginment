4cc51a3c0106be0e1f896ee86ee566edf753397e
jayin301 <mocajay.jh@gmail.com>
2023-02-11 17:42:44 +0900

    feat: init logger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express, { Application, Request, Response } from "express";
-import YAML from "yamljs";
+import logger from "./config/logger";
+import morgan from "morgan";
 import dotenv from "dotenv";
 import AppRoutes from "./modules/app/app.route";
 import "./config/mogoose";
 import "./config/redis";
 import { swaggerUi, specs } from "./swagger/swagger";
-import path from "path";
 
 // configurations
 dotenv.config();
-
 const app: Application = express();
 const port = process.env.PORT || 3000;
 const base: string = process.env.base_url ?? "/api/v1";
+const combined =
+  ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
+const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined;
+
 // const swaggerSpec = YAML.load(
 //   path.join(__dirname, "../src/swagger/swagger.yaml/")
 // );
@@ -21,6 +24,7 @@ const base: string = process.env.base_url ?? "/api/v1";
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use(morgan(morganFormat, { stream: logger.stream }));
 
 // Application routing
 app.get("/", (req: Request, res: Response) => {
@@ -29,13 +33,13 @@ app.get("/", (req: Request, res: Response) => {
 app.use(base, AppRoutes);
 
 // Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+app.listen(port, () => logger.info(`Server is listening on port ${port}!`));
 
 // Handle unhandled promise rejections and exceptions
 process.on("unhandledRejection", (err: any) => {
-  console.log(err);
+  logger.error(err);
 });
 
 process.on("uncaughtException", (err: any) => {
-  console.log(err.message);
+  logger.error(err.message);
 });
