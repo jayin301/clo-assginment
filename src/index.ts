3a8489b0bd09906d13c8fa83c88226c087d1389a
jayin301 <mocajay.jh@gmail.com>
2023-02-11 18:53:56 +0900

    fix: fix swagger

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
 import express, { Application, Request, Response } from "express";
 import logger from "./config/logger";
 import morgan from "morgan";
+import YAML from "yamljs";
 import dotenv from "dotenv";
 import AppRoutes from "./modules/app/app.route";
 import "./config/mogoose";
 import "./config/redis";
 import { swaggerUi, specs } from "./swagger/swagger";
+import path from "path";
 
 // configurations
 dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 3000;
-const base: string = process.env.base_url ?? "/api/v1";
+const base: string = process.env.base_url ?? "/api";
 const combined =
   ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
 const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined;
-
-// const swaggerSpec = YAML.load(
-//   path.join(__dirname, "../src/swagger/swagger.yaml/")
-// );
-
+const swaggerYaml = YAML.load(path.join(__dirname, "../../swagger.yaml"));
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerYaml));
 app.use(morgan(morganFormat, { stream: logger.stream }));
 
 // Application routing
