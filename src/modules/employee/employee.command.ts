193cd2a58403c708aafd8a8c5c01c1bf476d51a7
jayin301 <mocajay.jh@gmail.com>
2023-02-14 15:40:17 +0900

    refactor: update validate logic
    - move duplicated email check to validator function
    - change unique field to 'name'
    - add null check to every data key

--- src/modules/employee/employee.command.ts
+++ src/modules/employee/employee.command.ts
@@ -1,15 +1,9 @@
-import client from "../../config/redis";
 import eventHandler from "../app/eventHandler";
-import getEmployeeModel from "./models/getEmployee";
 import { AddEmployeeDTO } from "./employee.types";
-import logger from "../../config/logger";
-import { BadRequestError } from "../app/errorHandler";
 
 class EmployeeCommandHandler {
   async addEmployee(payload: AddEmployeeDTO) {
     const { name, email, tel, joined } = payload;
-    const employee = await getEmployeeModel.findOne({ email }).lean();
-    if (employee) throw new BadRequestError("Email already exists");
     const employeeData = {
       name,
       email,
